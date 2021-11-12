3f6ffc8d23b22c8611f6c9c3a42ae757d6ab9c0e
Gasser Abdeslam <gannyser@gmail.com>
2021-11-12 15:33:59 -0500

    Primer Commit del proyecto

---
+++ src/App.java
@@ -0,0 +1,27 @@
+import Interfaz.Interfaz;
+
+/**
+* <h1>Un bingo con interfaz gráfica</h1>
+* BINGO......(El mucho texto)
+* <p>
+* <b>Note:</b> Giving proper comments in your program makes it more
+* user friendly and it is assumed as a high quality code.
+*
+* @author  Gasser Abdeslam Abdelgelil Serna ---- Santiago Marin Dussan
+* @version 1.0
+* @since   2021
+*/
+
+public class App {
+
+    //EL TAMAÑO DE LA INTERFAZ
+    public static final int ANCHO = 700;
+    public static final int ALTO = 650;
+
+    public static Interfaz interfaz = new Interfaz(ANCHO, ALTO); //Definir un objeto tipo Interfaz (swing)
+    public static void main(String[] args) throws Exception {
+        //Generación de labels
+        interfaz.generarLabel("Es un bingo", 80, 20);
+        interfaz.generarLabel("Otro bingo", 80, 40);
+    }
+}
---
+++ src/Interfaz/Interfaz.java
@@ -0,0 +1,70 @@
+package Interfaz;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Interfaz extends JFrame implements ActionListener{
+    
+    private JButton button_close;
+    private JButton button_compra;
+
+    /**
+     * Constructor
+     * @param ancho el ancho de la ventana
+     * @param alto la altura de la ventana
+     */
+    public Interfaz(int ancho, int alto){
+        setLayout(null);
+        //Genera los parametros iniciales de la interfaz (x.inicio, y.inicio, ancho, alto)
+        setBounds(0, 0, ancho, alto);
+        //Hace que siempre inicie en el centro de la pantalla
+        setLocationRelativeTo(null);
+        setResizable(false); //No se puede modificar el tamaño de la interfaz
+        setVisible(true); //Pinta la interfaz en pantalla
+
+        //Boton de cerrado
+        button_close = crearButton("Cerrar", 550, 550, 100, 30);
+
+        //Boton de compra
+        button_compra = crearButton("Comprar Boleto", 50, 550, 150, 30);
+    }
+
+    /**
+     * Genera un label
+     * @param mensja lo que se deasea visualizar en pantalla
+     * @param x la coordenada en x en la interfaz
+     * @param y la coordenada en y en la interfaz
+     */
+    public void generarLabel(String mensja, int x, int y){
+        JLabel label = new JLabel(mensja);
+        label.setBounds(x, y, 300, 30);
+        add(label);
+    }
+
+    /**
+     * Generación de un boton
+     * @param mensaje mensaje del boton
+     * @param x coordenada en x en la interfaz
+     * @param y coordenada en y en la interfaz
+     * @param ancho ancho del boton
+     * @param largo el largor del boton
+     * @return un boton con las caracteristicas mandadas para pintarlo en la interfaz
+     */
+    public JButton crearButton(String mensaje, int x, int y, int ancho, int largo){
+        JButton button = new JButton(mensaje);
+        button.setBounds(x, y, ancho, largo);
+        add(button);
+        button.addActionListener(this); //Evento del boton
+        return button;
+    }
+
+    //
+    //VENTOS
+    //
+    public void actionPerformed(ActionEvent event){
+        if(event.getSource() == button_close){
+            System.exit(0); //Cerrar el programa
+        } else if(event.getSource() == button_compra){
+            System.out.println("Desde el boton de compra");; //Cerrar el programa
+        }
+    }
+}
